a3cfd6e922612bcaef76c0fbfe9292e47673a0b0
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-07-30 20:54:35 +0530

    Wrote config.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="StoneCharioteerBot",
     version="0.1dev",
-    description=("A telegram bot with plugins.")
+    description=("A telegram bot with plugins."),
     long_description=open("README.md").read(),
     license="MIT",
     author="Vinay Keerthi",
@@ -16,6 +16,5 @@ setup(
         "Bug Tracker": "https://vinay87.github.io/stonecharioteerbot",
     },
     scripts=["launch_stonecharioteer_bot.py"],
-    packages=find_packages(),
-    requires=["APScheduler","requests","asyncio","python-telegram-bot","nltk","arrow","celery","sqlalchemy","redis"]
+    packages=find_packages()
 )
