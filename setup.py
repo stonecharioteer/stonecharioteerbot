2c4e466b2cfa190a9a61968f1bbec289b7774bd9
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-07-30 20:44:22 +0530

    Using setuptools instead of distutils.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from distutils import setup
+from setuptools import setup, find_packages
 
 setup(
     name="StoneCharioteerBot",
     version="0.1dev",
+    description=("A telegram bot with plugins.")
     long_description=open("README.md").read(),
+    license="MIT",
     author="Vinay Keerthi",
     author_email="ktvkvinaykeerthi@gmail.com",
-    packages="stonecharioteerbot",
+    keywords="telegram bot plugins",
+    url="https://vinay87.github.io/stonecharioteerbot",
+    project_urls={
+        "Bug Tracker": "https://github.com/vinay87/stonecharioteerbot/issues",
+        "Source Code": "https://github.com/vinay87/stonecharioteerbot",
+        "Bug Tracker": "https://vinay87.github.io/stonecharioteerbot",
+    },
+    scripts=["launch_stonecharioteer_bot.py"],
+    packages=find_packages(),
     requires=["APScheduler","requests","asyncio","python-telegram-bot","nltk","arrow","celery","sqlalchemy","redis"]
 )
