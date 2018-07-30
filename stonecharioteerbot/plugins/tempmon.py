40b55a11a8512671924802385a8dce3634b43065
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2018-07-30 22:04:26 +0530

    Added temperature API.

---
+++ stonecharioteerbot/plugins/tempmon.py
@@ -0,0 +1,17 @@
+import os
+import sqlite3
+import pandas as pd
+import datetime
+
+def get_df():
+    with sqlite3.connect(os.environ["TEMPMON_CACHE_DB"]) as con:
+        df = pd.read_sql("SELECT * from records",con)
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    return df
+
+def filter_last_one_day():
+    df = get_df()
+    last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60))
+    df_one_day = df.loc[df["timestamp"]>=last_day]
+    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df_one_day
--- stonecharioteerbot/stonecharioteer.py
+++ stonecharioteerbot/stonecharioteer.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import os
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                 level=logging.DEBUG)
 
+import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters
+import bs4
+
+from .plugins import tempmon
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.id, text="Hello, world!")
@@ -23,6 +29,26 @@ def categorize(bot, update):
         return True
     elif "fortune" in query:
         fortune(bot, update)
+        return True
+    elif "stats" in query or "statistics" in query:
+        df_one_day = tempmon.filter_last_one_day()
+        df_md = repr(df_one_day.describe())
+        send(bot, update, df_md,
+                parse_mode=telegram.ParseMode.MARKDOWN)
+    elif "temperature" in query:
+        df_one_day = tempmon.filter_last_one_day()
+        message = "Average Temperature in the past 24h was \n{:.4f}°C (Using Pressure as a reference)\n{:.4f} °C (using humidity as a reference)".format(df_one_day["temperature_p"].mean(), df_one_day["temperature_h"].mean())
+        send(bot, update, message)
+        return True
+    elif "humidity" in query:
+        df_one_day = tempmon.filter_last_one_day()
+        message = "Average humidity in the past 24h was {:.4f}%".format(df_one_day["humidity"].mean())
+        send(bot, update, message)
+        return True
+    elif "pressure" in query:
+        df_one_day = tempmon.filter_last_one_day()
+        message = "Average pressure in the past 24h was {:.4f} bar".format(df_one_day["pressure"].mean())
+        send(bot, update, message)
         return True
     return None
 
@@ -44,8 +70,11 @@ def cowsay_fortune(bot, update):
     out = subprocess.check_output(["cowsay", out.decode("ascii")])
     return send(bot, update, out.decode("ascii"))
 
-def send(bot, update, message):
-    bot.send_message(chat_id=update.message.chat_id, text=message)
+def send(bot, update, message, parse_mode=None):
+    if parse_mode is None:
+        bot.send_message(chat_id=update.message.chat_id, text=message)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text=message, parse_mode=parse_mode)
 
 def response(bot, update):
     response_attempt = categorize(bot, update)
