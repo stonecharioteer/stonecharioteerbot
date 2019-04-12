6604229a42acae5fdde71f668e30d185709050ad
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-04-12 07:20:34 +0530

    Added failsafe for the pressure. There are some anomolous values here.

--- stonecharioteerbot/plugins/tempmon.py
+++ stonecharioteerbot/plugins/tempmon.py
@@ -12,33 +12,33 @@ def get_df():
 def filter_last_one_day():
     df = get_df()
     last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60))
-    df_one_day = df.loc[df["timestamp"]>=last_day]
-    df_one_day = df.loc[df["pressure"]>=895]
-    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    df = df.loc[df["timestamp"]>=last_day]
+    df = df.loc[df["pressure"]>=800]
+    df = df[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
     return df_one_day
 
 def filter_last_one_week():
     df = get_df()
     last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60*7))
-    df_one_day = df.loc[df["timestamp"]>=last_day]
-    df_one_day = df.loc[df["pressure"]>=895]
-    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
-    return df_one_day
+    df = df.loc[df["timestamp"]>=last_day]
+    df = df.loc[df["pressure"]>=800]
+    df = df[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df
 
 def filter_last_one_month():
     df = get_df()
     last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60*30))
-    df_one_day = df.loc[df["timestamp"]>=last_day]
-    df_one_day = df.loc[df["pressure"]>=895]
-    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
-    return df_one_day
+    df = df.loc[df["timestamp"]>=last_day]
+    df = df.loc[df["pressure"]>=895]
+    df = df[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df
 
 def filter_last_six_months():
     df = get_df()
     last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60*30*6))
-    df_one_day = df.loc[df["timestamp"]>=last_day]
-    df_one_day = df.loc[df["pressure"]>=895]
-    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
-    return df_one_day
+    df = df.loc[df["timestamp"]>=last_day]
+    df = df.loc[df["pressure"]>=895]
+    df = df[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df
 
 
