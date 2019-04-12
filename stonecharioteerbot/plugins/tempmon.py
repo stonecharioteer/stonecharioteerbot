6aad464fee2263e1943f3cfb75c0bb208958b6d2
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-04-12 07:16:45 +0530

    Added failsafe for the pressure. There are some anomolous values here.

--- stonecharioteerbot/plugins/tempmon.py
+++ stonecharioteerbot/plugins/tempmon.py
@@ -13,5 +13,32 @@ def filter_last_one_day():
     df = get_df()
     last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60))
     df_one_day = df.loc[df["timestamp"]>=last_day]
+    df_one_day = df.loc[df["pressure"]>=895]
     df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
     return df_one_day
+
+def filter_last_one_week():
+    df = get_df()
+    last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60*7))
+    df_one_day = df.loc[df["timestamp"]>=last_day]
+    df_one_day = df.loc[df["pressure"]>=895]
+    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df_one_day
+
+def filter_last_one_month():
+    df = get_df()
+    last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60*30))
+    df_one_day = df.loc[df["timestamp"]>=last_day]
+    df_one_day = df.loc[df["pressure"]>=895]
+    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df_one_day
+
+def filter_last_six_months():
+    df = get_df()
+    last_day = (datetime.datetime.now() - datetime.timedelta(minutes=24*60*30*6))
+    df_one_day = df.loc[df["timestamp"]>=last_day]
+    df_one_day = df.loc[df["pressure"]>=895]
+    df_one_day = df_one_day[["timestamp","temperature_h","temperature_p","humidity","pressure"]]
+    return df_one_day
+
+
